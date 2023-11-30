716c9285091a873cf3b83754b1ba93c17c8e337f
bruno-kwemoi <bsaekwo@gmail.com>
2023-11-30 01:16:08 +0000

    Added feature to include external libraries through git submodule

--- app/main.cc
+++ app/main.cc
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "cuslib.h"
 #include "config.hpp"
+#include <nlohmann/json.hpp>
+// #include <httplib/httplib.h>
 
 int main()
 {
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
     std::cout << git_sha << '\n';
+
+    std::cout << "JSON: " << NLOHMANN_JSON_VERSION_MAJOR << "."
+              << NLOHMANN_JSON_VERSION_MINOR << "."
+              << NLOHMANN_JSON_VERSION_PATCH << '\n';
     print_hello_world();
     return 0;
 }
