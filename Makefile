f32518e0d4f6e995e6c02ac05c9f12a8abc97701
Wellnajapan <info@wellna.jp>
2023-11-30 13:55:14 +0900

    Added Doxygen Documentation

--- Makefile
+++ Makefile
@@ -5,3 +5,4 @@ prepaire:
 
 dependency:
 	cd build && cmake .. --graphviz=graph.dot && dot -Tpng graph.dot -o graph.png
+
--- src/cuslib/cuslib.cc
+++ src/cuslib/cuslib.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "cuslib.h"
 
+/**
+ * @brief Prints out hello world and tests the Json lib
+ *
+ * */
 void print_hello_world()
 {
     std::cout << "Hello World\n";
